9389dc81a4ef5b975f328806ab110ea9bf7886cb
BGutierrez04 <gutbilling@protonmail.com>
2026-09-21 19:10:02 -0600

    Final Submission

--- src/AuthContext.jsx
+++ src/AuthContext.jsx
@@ -7,42 +7,58 @@ const AuthContext = createContext();
 export function AuthProvider({ children }) {
   const [token, setToken] = useState();
   const [location, setLocation] = useState("GATE");
-  const [message, setMessage] = useState()
+  const [message, setMessage] = useState();
 
   // TODO: signup
-  const signup = async (newUser)=>{
+  const signup = async (newUser) => {
     try {
-      const req = await fetch('https://fsa-jwt-practice.herokuapp.com/signup', {
-        method:"POST",
-        headers :{ 
-                  "Content-Type": "application/json" 
-                },
-        body:JSON.stringify(newUser)
-      })
-      const res = await req.json()
-      setToken(res.token)
-      setMessage(res.message)
-      setLocation("TABLET")
+      const req = await fetch("https://fsa-jwt-practice.herokuapp.com/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newUser),
+      });
+
+      if (!req.ok) {
+        throw new Error("Signup Failed.");
+      }
+
+      const res = await req.json();
+      setToken(res.token);
+      setMessage(res.message);
+      setLocation("TABLET");
     } catch (error) {
-      console.log(error.message)
+      console.log(error.message);
     }
-  }
+  };
 
   // TODO: authenticate
 
-  const authenticate = async()=>{
-    const req = await fetch('https://fsa-jwt-practice.herokuapp.com/authenticate',  { 
-        method: "GET", 
-        headers: { 
-          "Content-Type": "application/json",
-          "Authorization": `Bearer ${token}` 
-        }
-    })
-    const data = await req.json()
-    setMessage(data.message)
-    setLocation("TUNNEL")
+  const authenticate = async () => {
+    if (!token) {
+      throw new Error("No Token Found.");
+    }
 
-  }
+    const req = await fetch(
+      "https://fsa-jwt-practice.herokuapp.com/authenticate",
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    );
+
+    if (!req.ok) {
+      throw new Error("Authentication failed.");
+    }
+
+    const data = await req.json();
+    setMessage(data.message);
+    setLocation("TUNNEL");
+  };
 
   const value = { location, signup, authenticate };
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
